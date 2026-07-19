2a4f3da992399967979da4a2a3607b15a30cbf7e
Dmytro Zykov <dmytri.zykov@gmail.com>
2026-07-19 21:36:49 +0200

    refactor: rename TaskStatus enum

--- src/task.rs
+++ src/task.rs
@@ -4,11 +4,11 @@ use serde::{Deserialize, Serialize};
 pub struct Task {
     pub id: u32,
     pub title: String,
-    pub status: Status,
+    pub status: TaskStatus,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub enum Status {
+pub enum TaskStatus {
     Todo,
     InProgress,
     Done,
