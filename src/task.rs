86f0fde8b48f5fa21533fd1e574fd95649cf2736
Dmytro Zykov <dmytri.zykov@gmail.com>
2026-07-19 21:18:21 +0200

    feat: implement Task model

---
+++ src/task.rs
@@ -0,0 +1,15 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Task {
+    pub id: u32,
+    pub title: String,
+    pub status: Status,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Status {
+    Todo,
+    InProgress,
+    Done,
+}
